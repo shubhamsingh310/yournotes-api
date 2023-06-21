2ea6f1d9ca9999131746e497752a618062736c38
Shubham Singh <shubhamsingh3102002s@gmail.com>
2023-06-21 19:56:19 +0530

    final

---
+++ server.js
@@ -0,0 +1,34 @@
+require('./models/db');
+const appConfig = require('./config/appConfig');
+const http = require('http');
+
+const PORT = appConfig.PORT;
+
+process.on('uncaughtException',(err)=>{
+    console.log("Uncaught Exception! Shutting down server...");
+    console.log(err.name,err.message);
+    process.exit(1);
+})
+
+const app = require('./app');
+
+const server = http.createServer(app);
+server.listen(PORT, _ =>{
+    console.log(`Server running on PORT ${PORT}`)
+});
+
+process.on('unhandledRejection',(err)=>{
+    console.log("Unhandled Rejection (Some Promise is unresolved)! Shutting down server...");
+    console.log(err.name,err.message);
+    server.close(()=>{
+        process.exit(1);
+    })
+});
+
+process.on('SIGTERM',()=>{
+    console.log("SIGTERM Received. Shutting down server gracefully...");
+    server.close(()=>{
+        console.log('Process Terminated...');
+    })
+})
+
